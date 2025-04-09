2a7b67446c304e6e69de8902cc286858d5dbe518
Felipe Afanador <rbeltran503@unab.edu.co>
2025-04-09 16:15:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# My Kotlin Jetpack Compose Project - StoreApp
+
+<div align="center">
+    <img src="blob:https://web.whatsapp.com/5148283f-a443-43f2-8e23-906071634284" alt="Foto de Felipe Afanador" width="300">
+
+
+
+# Studen Information
+## Name: Felipe Afanador Median
+## ID: U00182571
+
+# Universidad autonoma de Bucaramanga
---
+++ app/src/main/java/Felipe/Afanador/Medina/ui/theme/LoginScreen.kt
@@ -0,0 +1,98 @@
+package Felipe.Afanador.Medina.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview
+@Composable
+fun LoginScreen(){
+    Scaffold { innerPadding->
+        Column (
+            modifier = Modifier.
+                padding(innerPadding).
+                fillMaxSize().
+                padding(end = 20.dp, start = 20.dp),
+            horizontalAlignment =Alignment.CenterHorizontally
+        ){
+            Image(
+                imageVector = Icons.Default.Person,
+                contentDescription = null,
+                modifier = Modifier.size(150.dp),
+                colorFilter = ColorFilter.tint(Color(0xFFFF9900))
+
+
+            )
+
+            Text(
+                text = "Inician Sesión",
+                color =Color(0xFFFF9900),
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(Icons.Default.Email, contentDescription = null)
+                },
+                label = {
+                    Text(text = "Correo Electronico")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(Icons.Default.Lock, contentDescription = null)
+                },
+                label = {
+                    Text(text = "Contraseña")
+                },
+                shape = RoundedCornerShape(12.dp)
+            )
+            Button(onClick = {},
+                modifier = Modifier.
+                fillMaxWidth().
+                height(50.dp
+
+            )) {
+                Text(text = "Iniciar sesión"),
+
+            }
+
+        }
+
+    }
+}
